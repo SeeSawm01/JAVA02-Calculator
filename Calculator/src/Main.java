11cacf611179b591d125707134cf6fd92918a85b
Lee Min Sung <minsung2556@cju.ac.kr>
2025-10-01 11:08:04 +0900

    hotfix

---
+++ Calculator/src/Main.java
@@ -0,0 +1,27 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc_add = new Calculator();
+
+        calc_add.setNumbers(5, 3);
+        calc_add.add();
+        calc_add.displayResult("+");
+
+        //setNumbers 랑 displayResult 는 있으니 add 라인만 짜면 됨
+        Calculator calc_sub = new Calculator();
+        calc_sub.setNumbers(10, 20);
+        calc_sub.subtract();
+        calc_sub.displayResult("-");
+
+        Calculator calc_mul = new Calculator();
+        calc_mul.setNumbers(5, 3);
+        calc_mul.multiply();
+        calc_mul.displayResult("*");
+
+        Calculator calc_div = new Calculator();
+        calc_div.setNumbers(10, 2);
+        calc_div.divide();
+        calc_div.displayResult("/");
+    }
+}
